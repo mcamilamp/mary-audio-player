959ab927acb297365da85f55b7490557dfbfbef0
mcamilamp <camimerpa@gmail.com>
2025-04-11 11:45:45 -0500

    Add initial structure and styles for AudioPlayer component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AudioPlayer from "./components/AudioPlayer";
+
+function App() {
+  return (
+    <div className="container">
+      <AudioPlayer audioSrc="./audio/[선재 업고 튀어 (Lovely Runner) OST Part 1] 이클립스 (ECLIPSE) - 소나기 (Sudden Shower) MV.mp3" />
+    </div>
+  );
+}
+
+export default App;
